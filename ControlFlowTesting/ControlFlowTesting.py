39e196fe4e63085c28ec2eb110d8d2d25c2a5d20
Vu Tuan Hung <vutuanhung.fcbs@gmail.com>
2022-10-16 23:44:42 +0700

    control flow testing

---
+++ ControlFlowTesting/ControlFlowTesting.py
@@ -0,0 +1,28 @@
+import unittest
+from Function import electricity_to_money
+
+
+class TestEquivalencePartitioning(unittest.TestCase):
+    def test_electricity_to_money_case1(self):
+        self.assertEqual(electricity_to_money(1.0), -1)
+
+    def test_electricity_to_money_case2(self):
+        self.assertEqual(electricity_to_money(-10), -1)
+
+    def test_electricity_to_money_case3(self):
+        self.assertEqual(electricity_to_money(17), 30808)
+
+    def test_electricity_to_money_case4(self):
+        self.assertEqual(electricity_to_money(51), 92485)
+
+    def test_electricity_to_money_case5(self):
+        self.assertEqual(electricity_to_money(200), 401760)
+
+    def test_electricity_to_money_case6(self):
+        self.assertEqual(electricity_to_money(255), 552398)
+
+    def test_electricity_to_money_case7(self):
+        self.assertEqual(electricity_to_money(333), 776652)
+
+    def test_electricity_to_money_case8(self):
+        self.assertEqual(electricity_to_money(567), 1509634)
--- FunctionalTesting/FuntionalTesting.py
+++ FunctionalTesting/FuntionalTesting.py
@@ -1,22 +1,5 @@
 import unittest
-
-
-def electricity_to_money(e):
-    if not isinstance(e, int) or e < 0:
-        return -1
-    elif e <= 50:
-        money = (e * 1678) * 1.08
-    elif e <= 100:
-        money = (83900 + (e - 50) * 1734) * 1.08
-    elif e <= 200:
-        money = (170600 + (e - 100) * 2014) * 1.08
-    elif e <= 300:
-        money = (372000 + (e - 200) * 2536) * 1.08
-    elif e <= 400:
-        money = (625600 + (e - 300) * 2834) * 1.08
-    else:
-        money = (909000 + (e - 400) * 2927) * 1.08
-    return round(money)
+from Function import electricity_to_money
 
 
 class TestBoundaryValue(unittest.TestCase):
